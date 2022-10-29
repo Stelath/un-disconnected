1b21b590dff666c4ef26b780a36ece00a2c8e326
Alex <alex.k.korte@gmail.com>
2022-10-29 10:27:56 -0400

    10:27 AM I think im going to die
    Co-authored-by: tate007 <tate007@users.noreply.github.com>
    Co-authored-by: Camden Spehl <TheGoldenWaffle@users.noreply.github.com>
    Co-authored-by: TheMustachio <TheMustachio@users.noreply.github.com>
    Co-authored-by: AFDtea <AFDtea@users.noreply.github.com>

--- snake/src/App.js
+++ snake/src/App.js
@@ -22,21 +22,112 @@ function App() {
     </div>
   )
 }
+//move this to snake?
 const  HEIGHT = 20;
 const  WIDTH  = 20;
 
+const emptyRows = () => [...Array(WIDTH)].map((_) => [...Array(HEIGHT)].map((_)=> 'grid-item'));
+
+const getRandomFood = () => {
+  return  { 
+      x: Math.floor(Math.random() *WIDTH),
+      y: Math.floor(Math.random() *HEIGHT) 
+  }
+}
+
 const initialState = {
-  rows:1 em1, 1
-  snake2: [WIDTH-2],
-  snake3: [],
-  snake4: [],
-  snake: [getRandom()],
-  food: getRandom(),
-  direction: STOP,
+  rows: emptyRows(),
+  snake1: {direction: STOP, x:1, y:1},
+  snake2: {direction: STOP, x:WIDTH-2, y:1},
+  snake3: {direction: STOP, x:1, y:HEIGHT-2},
+  snake4: {direction: STOP, x:WIDTH-2, y:HEIGHT-2},
+  food1: getRandomFood(),
   speed: 100,
-
-
 };
+
+//  isCollapsed = (selfSnake) => {
+//         let snake = selfSnake;
+//         let head  = {...snake[snake.length-1]} 
+//         for (let i=0; i<snake.length-3; i++) {// checking itself
+//             if ((head.x === snake[i].x) &&(head.y === snake[i].y)) {
+//                 this.setState(initialState);
+//                 alert(`game over: ${snake.length*10}`)
+//             }
+//         }
+//     }
+
+isCollideSelf = (selfSnake) => {
+        let head  = {...selfSnake[selfSnake.length-1]} 
+        for (let i=0; i<selfSnake.length-3; i++) {// checking itself
+            if ((head.x === selfSnake[i].x) &&(head.y === selfSnake[i].y)) {
+                this.setState(initialState);
+                alert(`game over: ${selfSnake.length*10}`)//ending state 
+            }
+        }
+  }
+isCollidingOthers = (selfSnake, snak1, snak2, snak3) => {
+  let selfHead = {...selfSnake[selfSnake.length-1]}
+  for (let i = 0; i<selfHead; i++){
+    if((selfHead.x === snak1[i].x) && (selfHead.y === snak1[i].y)){
+      this.setState(initialState);
+      alert(`game over: ${selfSnake.length*10}`)//ending state 
+    }
+  }
+  for (let i = 0; i<selfHead; i++){
+    if((selfHead.x === snak2[i].x) && (selfHead.y === snak2[i].y)){
+      this.setState(initialState);
+      alert(`game over: ${selfSnake.length*10}`)//ending state 
+    }
+  }
+  for (let i = 0; i<selfHead; i++){
+    if((selfHead.x === snak3[i].x) && (selfHead.y === snak3[i].y)){
+      this.setState(initialState);
+      alert(`game over: ${selfSnake.length*10}`)//ending state 
+    }
+  }
+}
+
+changeDirection = ({keyCode}) => { 
+  let direction = this.state.direction;
+  switch (keyCode) {
+      case LEFT:
+          direction = (direction === RIGHT)? RIGHT: LEFT;
+          break;
+      case RIGHT:
+          direction = (direction === LEFT)? LEFT: RIGHT;
+          break;
+      case UP:
+          direction = (direction === DOWN)? DOWN: UP;
+          break;
+      case DOWN:
+          direction = (direction === UP)? UP: DOWN;
+          break;
+      case STOP:
+          direction = STOP;
+          break;
+      default:
+          break;
+  }
+  this.setState({
+      direction: direction
+  });
+}
+
+render() {
+  const displayRows = this.state.rows.map((row, i) => row.map((value, j) =>  <div name={`${i}=${j}`} className={value} />))
+  return (
+      <div className="a">
+          <h1> Snake  v0.1.1</h1>
+          
+              {/* <li>press "space" to pause the game.</li>
+              <li>press "arrow keys" to change direction/ unpause.</li> */}
+         
+          <div className="snake-container">
+              <div className="grid">{displayRows}</div>
+          </div>
+      </div>
+  )    
+}
 
 class SnakeGame extends Component{
   constructor(props){
@@ -45,6 +136,7 @@ class SnakeGame extends Component{
     this.state = initialState;
   }
 } ;
+
 
 
 export default App;
