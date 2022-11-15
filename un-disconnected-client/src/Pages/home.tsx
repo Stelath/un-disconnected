8c73cd0f1ba8a93b860795a69e63b59b6ab7c356
Alex <alex.k.korte@gmail.com>
2022-11-15 17:49:41 -0500

    Added logo + more typescript conversion

--- un-disconnected-client/src/Pages/home.tsx
+++ un-disconnected-client/src/Pages/home.tsx
@@ -2,32 +2,37 @@ import React from "react";
 import theme from "../theme/theme";
 
 import { ThemeProvider } from "@mui/material/styles";
-import { Typography, Stack, Button } from "@mui/material";
+import { Typography, Stack, Button, Box } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 
 function Home() {
   return (
+    <Box >
     <ThemeProvider theme={theme}>
-      <Stack
-        mt={5}
-        direction="column"
-        justifyContent="center"
-        alignItems="center"
-        spacing={4}
-      >
-        <Typography variant="h1"> un-Disconnected </Typography>
-        <Typography variant="h1">
-          WEB BASED
-          PARTY GAMES
-        </Typography>
-        <Button variant="contained" href="/join">
-          Join A Room
-        </Button>
-        <Button variant="contained" href="/createroom">
-          {" "}
-          Create a Room
-        </Button>
-      </Stack>
+      <Grid container spacing={2}>
+        <Grid xs={8}>
+          <img src="/banner.svg" alt="UnDisconnected Banner" />
+          <Typography variant="h1">WEB BASED</Typography>
+          <Typography variant="h1">PARTY GAMES</Typography>
+        </Grid>
+        <Stack
+          mt={5}
+          direction="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={4}
+        >
+          <Button variant="contained" href="/join">
+            Join A Room
+          </Button>
+          <Button variant="contained" href="/createroom">
+            {" "}
+            Create a Room
+          </Button>
+        </Stack>
+      </Grid>
     </ThemeProvider>
+    </Box>
   );
 }
 
